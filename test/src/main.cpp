b1a8a6de78782abd4582f4f074ee1f226ac8eb74
Naphtalene <francois.sestier@gmail.com>
2021-07-10 19:38:57 +0200

    Update main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -22,10 +22,14 @@ int main(int argc, char** argv) {
 TEST(EVENT,EVENT){
     epp::dispatcher eventHandler;
 
-    eventHandler.on<Event1>([](Event1 e){ ASSERT_EQ(e.a,1); ASSERT_EQ(e.b,2); });
+    eventHandler.on<Event1>([](Event1 e){ ASSERT_EQ(e.a,1); });
+    eventHandler.on<Event1>([](Event1 e){ ASSERT_EQ(e.b,2); });
+
     eventHandler.on<Event2>([](Event2 e){ ASSERT_EQ(e.s,'t'); });
+    eventHandler.on<Event3>([](Event3 e){ ASSERT_EQ(e.s,"test"); });
+
     eventHandler.emit<Event1>(1,2);
     eventHandler.emit<Event2>('t');
-    eventHandler.emit<Event3>("dstes");
+    eventHandler.emit<Event3>("test");
 
 }
