242d98ca60394868d75c322f74813afd8f1357a9
Francois Sestier <francois.sestier@gmail.com>
2021-08-10 10:36:51 +0200

    test for the new bus and event_queues

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,17 +2,13 @@
 
 #include <event-system.hpp>
 
-struct Event1 {
+struct A {
     int a;
     int b;
 };
 
-struct Event2 {
+struct B {
     char s;
-};
-
-struct Event3 {
-    std::string s;
 };
 
 int main(int argc, char** argv) {
@@ -20,69 +16,60 @@ int main(int argc, char** argv) {
     return RUN_ALL_TESTS();
 }
 
-TEST(Dispatcher, Event) {
-    epp::dispatcher eventHandler;
+using namespace epp;
 
+TEST(Dispatcher, Bus) {
+    epp::bus<event<A>> bus;
     int a = 0;
     int b = 0;
     std::string s;
 
-    eventHandler.on<Event1>([&](Event1 e) {
-        a = e.a;
-        ASSERT_EQ(e.a, 1);
-    });
-    eventHandler.on<Event1>([&](Event1 e) {
-        b = e.b;
-        ASSERT_EQ(e.b, 2);
-    });
-    eventHandler.on<Event2>([](Event2 e) { ASSERT_EQ(e.s, 't'); });
-    eventHandler.on<Event3>([&](Event3 e) {
-        s = e.s;
-        ASSERT_EQ(e.s, "test");
+    bus.attach_back<event<A>>([&](event<A> e) {
+        a = e->a;
+        e.consume();
+        ASSERT_EQ(e->a, 1);
     });
 
-    eventHandler.emit<Event1>(1, 2);
-    eventHandler.emit<Event2>('t');
-    eventHandler.emit<Event3>("test");
+    bus.attach_back<event<A>>([&](event<A> e) {
+        ASSERT_EQ(
+            1,
+            100);  // always false checking if event consuming is working right
+    });
 
+    bus.attach_front<event<A>>([&](event<A> e) { ASSERT_EQ(a, 0); });
 
-    ASSERT_EQ(a, 1);
-    ASSERT_EQ(b, 2);
-    ASSERT_EQ(s, "test");
+    bus.dipatch<event<A>>(1, 2);
 }
 
-TEST(Dispatcher, EventQueues) {
-    epp::dispatcher dispatcher;
-    epp::event_queue main_event_queue;
+TEST(Dispatcher, Queues) {
+    bus<event<A>> bus;
     int a = 0;
     int b = 0;
-    std::string s;
-
-    dispatcher.on<Event1>([&](Event1 e) {
-        a = e.a;
-        ASSERT_EQ(e.a, 1);
-    });
-    dispatcher.on<Event1>([&](Event1 e) {
-        b = e.b;
-        ASSERT_EQ(e.b, 2);
-    });
-    dispatcher.on<Event2>([](Event2 e) { ASSERT_EQ(e.s, 's'); });
-    dispatcher.on<Event3>([&](Event3 e) {
-        s = e.s;
-        ASSERT_EQ(e.s, "test");
+    event_queue<event<A>, event<B>> queue;
+    bus.attach_back<event<A>>([&](event<A> e) {
+        a = e->a;
+        e.consume();
+        ASSERT_EQ(e->a, 1);
     });
 
-    main_event_queue.push<Event1>(1, 2);
-    main_event_queue.push<Event2>('s');
-    main_event_queue.push<Event3>("test");
+    bus.attach_back<event<A>>([&](event<A> e) {
+        ASSERT_EQ(
+            1,
+            100);  // always false checking if event consuming is working right
+    });
 
-    ASSERT_EQ(main_event_queue.size(), 3);
+    bus.attach_front<event<A>>([&](event<A> e) { ASSERT_EQ(a, 0); });
 
-    dispatcher.emit(main_event_queue);
+    queue.push_back<event<A>>(1, 2);
 
-    ASSERT_TRUE(main_event_queue.empty());
+    ASSERT_EQ(queue.size(), 1);
+    ASSERT_FALSE(queue.empty());
 
-    ASSERT_EQ(a, 1);
-    ASSERT_EQ(b, 2);
-    ASSERT_EQ(s, "test");
+    bus.dipatch(queue);
+    ASSERT_EQ(queue.size(), 1);
+    bus.dipatch(queue);
+    queue.clear();
+    ASSERT_EQ(queue.size(), 0);
+    ASSERT_TRUE(queue.empty());
+
 }
