492b2cabf218d32dfd0363eac8accb5749cec3e5
Francois Sestier <francois.sestier@gmail.com>
2021-08-10 10:36:37 +0200

    reforged event-system to a strongly type approach improving performance, making event consumable and adding a bit complexity to the usage.

--- include/event-system.hpp
+++ include/event-system.hpp
@@ -37,134 +37,240 @@ namespace epp {
         inline static id_type type_id() {
             return details::type_id_builder::get<T>();
         }
+
+        template <typename T, typename... Ts>
+        struct are_distinct
+            : std::conjunction<std::negation<std::is_same<T, Ts>>...,
+                               are_distinct<Ts...>> {};
+
+        template <typename T>
+        struct are_distinct<T> : std::true_type {};
+
+        template <typename... Ts>
+        inline static constexpr bool are_distinct_v
+            = are_distinct<Ts...>::value;
+
+        template <typename T, typename... Ts>
+        struct contains : std::disjunction<std::is_same<T, Ts>...> {};
+
+        template <typename T, typename... Ts>
+        inline static constexpr bool contains_v = contains<T, Ts...>::value;
+
+        template <typename... Ts>
+        concept distinct = are_distinct_v<Ts...>;
+
+        template <class T, template <class...> class Template>
+        struct is_specialization : std::false_type {};
+
+        template <template <class...> class Template, class... Args>
+        struct is_specialization<Template<Args...>, Template> : std::true_type {
+        };
+
+        template <class T, template <class...> class Template>
+        inline static constexpr bool is_specialization_v
+            = is_specialization<T, Template>::value;
+
     }  // namespace details
 
-
-    class event_queue {
-
+    template <typename event_type>
+    class event {
        public:
-        using internal_event_queue = std::queue<std::pair<details::id_type, void*>>;
-        using event_dtors = std::unordered_map<details::id_type, details::dtor>;
+        using undelying_type = event_type;
 
-       public:
-        event_queue() = default; //ctor
+        event(event_type&& event) : event_(std::move(event)) {}
 
-        ~event_queue() { clear(); } //dtor
+        template<typename ...Args>
+        event(Args... args) : event_(event_type(std::forward<Args>(args)...)) {}
 
-        event_queue(const event_queue& other)  // copy ctor
-            : _event_queue(other._event_queue),
-              _event_dtors(other._event_dtors) {}
 
-        event_queue(event_queue&& other) noexcept //move ctor
-        {
-            std::swap(_event_queue,other._event_queue);
-            std::swap(_event_dtors,other._event_dtors);
-        }
+        void consume() { consumed_ = true; }
 
-        event_queue& operator=(const event_queue& other)  // copy assignment
-        {
-            return *this = event_queue(other);
-        }
+        event_type* operator->() { return &event_; }
 
-        event_queue& operator=(event_queue&& other) noexcept  // move assignment
-        {
-            return *this = event_queue(std::move(other));
-        }
+        event_type& operator*() { return event_; }
 
-        template <typename Event, typename... Args>
-        void push(Args... args) {
-            ensure_dtor<Event>();
-            _event_queue.push(std::make_pair<details::id_type,void*>(details::type_id<Event>(),
-                              new Event(std::forward<Args>(args)...)));
-        }
-
-        auto front() { return _event_queue.front(); }
-
-        void pop() {
-            auto&& event = _event_queue.front();
-            _event_dtors.at(event.first)(event.second); // calling appropriate dtor;
-            _event_queue.pop();
-        }
-
-        bool empty() { return _event_queue.empty(); }
-
-        void clear() {
-            while (!empty()) {
-                pop();
-            }
-        }
-
-        size_t size() { return _event_queue.size(); }
-
+        bool alive() const { return consumed_; }
 
        private:
-        template <typename Event>
-        void ensure_dtor() {
-            if (!_event_dtors.contains(details::type_id<Event>())) {
-                _event_dtors.emplace(details::type_id<Event>(), [](void* obj) {
-                    delete static_cast<Event*>(obj);
-                });
-            }
-        }
-
-       private:
-        internal_event_queue _event_queue;
-        event_dtors _event_dtors;
+        bool consumed_ = false;
+        event_type event_;
     };
 
-    class dispatcher {
+    template <typename T>
+    concept dispatchable = details::is_specialization_v<T, event>;
+
+    template <dispatchable... event_types>
+    requires(details::are_distinct_v<event_types...>) class event_queue {
        public:
-        using event_callback = std::function<void(void*)>;
-        using listners
-            = std::unordered_map<details::id_type, std::vector<event_callback>>;
+        using underlying_event_types = std::tuple<event_types...>;
+        using internal_event_queues = std::tuple<std::vector<event_types>...>;
 
-        template <class EventType, class ListenerCallback>
-        void on(ListenerCallback listener) {
-            if (!_listeners.contains(details::type_id<EventType>())) {
-                _listeners.try_emplace(details::type_id<EventType>(),
-                                       std::vector<event_callback>());
-            }
-            _listeners[details::type_id<EventType>()].push_back(
-                [=](void* event) {
-                listener(*static_cast<EventType*>(event));
-            });
+       public:
+        template <dispatchable T, typename... Args>
+        requires(details::contains_v<T, event_types...>) void push_back(
+            Args... args) {
+            std::get<std::vector<T>>(event_queue_)
+                .emplace_back(T(std::forward<Args>(args)...));
         }
 
-        template <class EventType, typename... Args>
-        void emit(Args... args) {
-            EventType event{std::forward<Args>(args)...};
-            emit_impl(event);
+        template <dispatchable T>
+        requires(details::contains_v<T, event_types...>) auto begin() {
+            return std::get<std::vector<T>>(event_queue_).begin();
         }
 
-        template <class EventType, typename... Args>
-        void emit(EventType&& event) {
-            emit_impl(event);
+        template <dispatchable T>
+        requires(details::contains_v<T, event_types...>) auto end() {
+            return std::get<std::vector<T>>(event_queue_).end();
         }
 
-        void emit(event_queue& queue) { 
-            while (!queue.empty()) {
-                emit_impl(queue.front().first, queue.front().second);
-                queue.pop();
-            }
+        bool empty() {
+            bool empty = true;
+            ((empty
+              &= std::get<std::vector<event_types>>(event_queue_).empty()),
+             ...);
+            return empty;
         }
 
-        void clear() { _listeners.clear(); }
+        void clear() {
+            ((std::get<std::vector<event_types>>(event_queue_).clear()), ...);
+        }
+
+        size_t size() {
+            size_t size(0);
+            ((size += std::get<std::vector<event_types>>(event_queue_).size()),
+             ...);
+            return size;
+        }
 
        private:
-        template <class EventType>
-        void emit_impl(EventType& event) {
-            emit_impl(details::type_id<EventType>(),&event);
+        internal_event_queues event_queue_;
+    };
+    template <dispatchable... event_types>
+    requires(details::are_distinct_v<event_types...>) class bus {
+       public:
+        template <dispatchable T>
+        using event_callback = std::function<void(T)>;
+
+        template <dispatchable T>
+        using event_callback_list = std::vector<event_callback<T>>;
+
+        using storage = std::tuple<event_callback_list<event_types>...>;
+
+        template <dispatchable T>
+        requires(details::contains_v<T, event_types...>) void attach_back(
+            event_callback<T> callback) {
+            std::get<event_callback_list<T>>(event_callback_list_)
+                .push_back(callback);
         }
 
-        void emit_impl(details::id_type id, void* event) {
-            if (_listeners.contains(id)) {
-                for (auto it : _listeners[id]) {
-                    it(event);
+        template <dispatchable T>
+        requires(details::contains_v<T, event_types...>) void attach_front(
+            event_callback<T> callback) {
+            auto& callbacks
+                = std::get<event_callback_list<T>>(event_callback_list_);
+            callbacks.insert(callbacks.begin(), 0);
+        }
+
+        template <dispatchable T>
+        requires(details::contains_v<T, event_types...>) void detach(
+            event_callback<T> callback) {
+            auto& callbacks
+                = std::get<event_callback_list<T>>(event_callback_list_);
+            auto it = std::find(callbacks.begin(), callbacks.end(), callback);
+            if (it != callbacks.end()) {
+                callbacks.erase(it);
+            }
+        }
+
+        template <dispatchable T, typename... Args>
+        requires(details::contains_v<T, event_types...>) void dipatch(
+            Args... args) {
+            T e(T::undelying_type(std::forward<Args>(args)...));
+            for (auto& callback : std::get<event_callback_list<T>>(event_callback_list_)) {
+                if (e.alive()) {
+                    callback(e);
                 }
             }
         }
 
+        template <dispatchable T>
+        void dipatch(T& e) {
+            if constexpr (details::contains_v<T, event_types...>) {
+                for (auto& callback : std::get<event_callback_list<T>>(event_callback_list_)) {
+                    if (e.alive()) {
+                        callback(e);
+                    }
+                }
+            }
+        }
+
+        template <dispatchable... Ts>
+        void dipatch(event_queue<Ts...>& e_queue) {
+            ((std::for_each(e_queue.begin<Ts>(), e_queue.end<Ts>(),
+                            [=](Ts& e) { dipatch(e); })),
+             ...);
+        }
+
        private:
-        listners _listeners;
+        storage event_callback_list_;
     };
+
+    /*
+        class dispatcher {
+           public:
+            using event_callback = std::function<void(void*)>;
+            using listners
+                = std::unordered_map<details::id_type,
+       std::vector<event_callback>>;
+
+            template <class EventType, class ListenerCallback>
+            void on(ListenerCallback listener) {
+                if (!_listeners.contains(details::type_id<EventType>())) {
+                    _listeners.try_emplace(details::type_id<EventType>(),
+                                           std::vector<event_callback>());
+                }
+                _listeners[details::type_id<EventType>()].push_back(
+                    [=](void* event) {
+                        listener(*static_cast<EventType*>(event));
+                    });
+            }
+
+            template <class EventType, typename... Args>
+            void emit(Args... args) {
+                EventType event{std::forward<Args>(args)...};
+                emit_impl(event);
+            }
+
+            template <class EventType, typename... Args>
+            void emit(EventType&& event) {
+                emit_impl(event);
+            }
+
+            void emit(event_queue& queue) {
+                while (!queue.empty()) {
+                    emit_impl(queue.front().first, queue.front().second);
+                    queue.pop();
+                }
+            }
+
+            void clear() { _listeners.clear(); }
+
+           private:
+            template <class EventType>
+            void emit_impl(EventType& event) {
+                emit_impl(details::type_id<EventType>(), &event);
+            }
+
+            void emit_impl(details::id_type id, void* event) {
+                if (_listeners.contains(id)) {
+                    for (auto it : _listeners[id]) {
+                        it(event);
+                    }
+                }
+            }
+
+           private:
+            listners _listeners;
+        };
+        */
 }  // namespace epp
