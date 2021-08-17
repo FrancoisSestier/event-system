e7175f278e10a19559e928914954ff3477cbbed0
Francois Sestier <francois.sestier@gmail.com>
2021-08-17 18:39:53 +0100

    actualizing tests

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -22,43 +22,57 @@ TEST(Dispatcher, Bus) {
     epp::bus<event<A>> bus;
     int a = 0;
     int b = 0;
+    int count = 0;
     std::string s;
 
     bus.attach_back<event<A>>([&](event<A>& e) {
+        count++;
         a = e->a;
         e.consume();
         ASSERT_EQ(e->a, 1);
     });
 
     bus.attach_back<event<A>>([&](event<A>& e) {
+        count++;
         ASSERT_EQ(
             1,
             100);  // always false checking if event consuming is working right
     });
 
-    bus.attach_front<event<A>>([&](event<A>& e){ ASSERT_EQ(a, 0); });
+    bus.attach_front<event<A>>([&](event<A>& e) {
+        count++;
+        ASSERT_EQ(a, 0);
+    });
 
     bus.dipatch<event<A>>(1, 2);
+    ASSERT_EQ(count, 2);
 }
 
 TEST(Dispatcher, Queues) {
     bus<event<A>> bus;
     int a = 0;
     int b = 0;
+    int count = 0;
+
     event_queue<event<A>, event<B>> queue;
     bus.attach_back<event<A>>([&](event<A>& e) {
+        count++;
         a = e->a;
         e.consume();
         ASSERT_EQ(e->a, 1);
     });
 
     bus.attach_back<event<A>>([&](event<A>& e) {
+        count++;
         ASSERT_EQ(
             1,
             100);  // always false checking if event consuming is working right
     });
 
-    bus.attach_front<event<A>>([&](event<A> e){ ASSERT_EQ(a, 0); });
+    bus.attach_front<event<A>>([&](event<A>& e) {
+        count++;
+        ASSERT_EQ(a, 0);
+    });
 
     queue.push_back<event<A>>(1, 2);
 
@@ -68,15 +82,15 @@ TEST(Dispatcher, Queues) {
     bus.dipatch(queue);
     ASSERT_EQ(queue.size(), 1);
     bus.dipatch(queue);
+    ASSERT_EQ(count, 2);
+    ASSERT_EQ(a,1);
     queue.clear();
     ASSERT_EQ(queue.size(), 0);
     ASSERT_TRUE(queue.empty());
-
 }
 
-TEST(Event,MakeBus) {
-    using some_event = event_type_list<event<A>,event<B>>;
+TEST(Event, MakeBus) {
+    using some_event = event_type_list<event<A>, event<B>>;
 
     auto bus = make_bus<some_event>();
-
 }
